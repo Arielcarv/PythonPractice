f90e98bb4fc501d26d03c3f03c8343742e54847f
Ariel Carvalho <arielcarvnasc@gmail.com>
2021-11-24 17:38:58 -0300

    Feat: Adding new polls page to project - 70%

--- Pollster/pollster/polls/admin.py
+++ Pollster/pollster/polls/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Question, Choice
+
+# Change Admin site Header
+admin.site.site_header = "Pollster App Admin"
+admin.site.site_title = "Pollster App Admin Area"
+admin.site.index_title = "Welcome to the Pollster App Admin"
 
 
 class ChoiceInLine(admin.TabularInline):
@@ -10,8 +13,8 @@ class ChoiceInLine(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {
-        'fields': ['publication_date'], 'classes': ['collapse']}), ]
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['publication_date'], 'classes': ['collapse']}), ]
     inlines = [ChoiceInLine]
 
 # admin.site.register(Question)
--- Pollster/pollster/polls/views.py
+++ Pollster/pollster/polls/views.py
@@ -1,3 +1,18 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Choice, Question
+from django.template import loader
 
 # Create your views here.
+# from django.http import HttpResponse
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-publication_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
