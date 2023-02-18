cfe1176d6441a248b25772703438d98de0d98509
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-02-18 01:08:11 -0300

    feat: add endpoint rock

--- Westeros/lannister/api.py
+++ Westeros/lannister/api.py
@@ -6,3 +6,8 @@ router = Router()
 @router.get("/home")
 def home(request):
     return "A lannister always pays their debts"
+
+
+@router.get("/rock")
+def rock(request, height:int):
+    return f"Casterly Rock is {height + 1}m tall"
