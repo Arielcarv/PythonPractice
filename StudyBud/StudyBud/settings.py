a7f1b4b7773c3606f5afd91b986a02b2d842f0b9
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-04-11 13:18:26 -0300

    feat: StudyBud - add & config django-cors-headers

--- StudyBud/StudyBud/settings.py
+++ StudyBud/StudyBud/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = "django-insecure-7=(y7^+fhm+!*#m&!1cv%w1*n_$&045y)a+eg8lvf_+(vzp&^s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGIN = True
 
 # Application definition
 
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "general.apps.GeneralConfig",
     "api.apps.ApiConfig",
 ]
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
