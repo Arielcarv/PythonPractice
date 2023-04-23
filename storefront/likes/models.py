8c5adefd4d5ec3f0d486644133e44fbcb6301531
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-04-22 21:23:11 -0300

    feat: Storefront - add likes models + admin objects

--- storefront/likes/models.py
+++ storefront/likes/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+
+class LikedItem(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
