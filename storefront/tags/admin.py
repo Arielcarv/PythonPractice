36a2f314cb92c6e9cf78d901dd3222f415a37b40
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-04-22 21:18:22 -0300

    feat: Storefront - add likes app

--- storefront/tags/admin.py
+++ storefront/tags/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from tags.models import Tag, TaggedItem
 
-admin.site.regiter(Tag)
-admin.site.regiter(TaggedItem)
+admin.site.register(Tag)
+admin.site.register(TaggedItem)
--- storefront/tags/models.py
+++ storefront/tags/models.py
@@ -7,15 +7,15 @@ class Tag(models.Model):
     label = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.label
 
 
 class TaggedItem(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    product = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey("product", "object_id")
+    content_object = GenericForeignKey("content-type", "object_id")
 
-    def __unicode__(self):
+    def __str__(self):
         return self.tag.label
