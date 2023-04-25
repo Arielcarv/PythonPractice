a8e4aab13a4e45dad21cdf34510ddfc091783b45
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-04-25 20:25:52 -0300

    WIP: Storefront - more query_sets.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 def home(request):
-    all_products = Product.objects.all()
-    return render(request, "home.html", {"products": all_products})
+    distinct_order_items = OrderItem.objects.values("product_id").distinct()
+    products = Product.objects.filter(id__in=distinct_order_items).order_by("title")
+    return render(request, "home.html", {"products": list(products)})
 
 
 def say(request):
