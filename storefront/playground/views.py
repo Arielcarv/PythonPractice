6520f6225271a7617338aad8877cc7396ecb1f1f
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-04-27 22:45:52 -0300

    WIP: Storefront - ORM aggregate results

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models.aggregates import Count, Max, Min, Avg
 from store.models import Product, OrderItem, Order
 
 
@@ -6,12 +7,17 @@ def home(request):
     """Select products that have been ordered and sort those products by title"""
     distinct_order_items = OrderItem.objects.values("product_id").distinct()
     products = Product.objects.filter(id__in=distinct_order_items).order_by("title")
+    
     """Get the last 5 orders with their customer and items (this includes product)"""
     last_5_orders = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
     
+    """Aggregation"""
+    aggregation_result = Product.objects.filter(collection__id=3).aggregate(count=Count("id"), min_price=Min("unit_price"))
+    
     context = {
         "products": list(products),
-        "last_5_orders": last_5_orders
+        "last_5_orders": last_5_orders,
+        "aggregation_result" : aggregation_result
     }
     return render(request, "home.html", context)
 
