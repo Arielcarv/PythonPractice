dc64268ecb4d20f1463ae24896205f4d52f9a01e
Ariel Carvalho Nascimento <arielcarvnasc@gmail.com>
2023-04-28 21:09:55 -0300

    feat: Storefront - ORM annotate result.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,23 +1,37 @@
+from django.db.models import Value, F
+from django.db.models.aggregates import Count, Min
 from django.shortcuts import render
-from django.db.models.aggregates import Count, Max, Min, Avg
-from store.models import Product, OrderItem, Order
+
+from store.models import Product, OrderItem, Order, Customer
 
 
 def home(request):
     """Select products that have been ordered and sort those products by title"""
     distinct_order_items = OrderItem.objects.values("product_id").distinct()
     products = Product.objects.filter(id__in=distinct_order_items).order_by("title")
-    
+
     """Get the last 5 orders with their customer and items (this includes product)"""
-    last_5_orders = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
-    
+    last_5_orders = (
+        Order.objects.select_related("customer")
+        .prefetch_related("orderitem_set__product")
+        .order_by("-placed_at")[:5]
+    )
+
     """Aggregation"""
-    aggregation_result = Product.objects.filter(collection__id=3).aggregate(count=Count("id"), min_price=Min("unit_price"))
-    
+    aggregation_result = Product.objects.filter(collection__id=3).aggregate(
+        count=Count("id"), min_price=Min("unit_price")
+    )
+
+    """Annotate"""
+    annotate_queryset = Customer.objects.annotate(
+        is_new=Value(True), new_id=F("id"), new_id_plus_1=F("id") + 1
+    )
+
     context = {
         "products": list(products),
         "last_5_orders": last_5_orders,
-        "aggregation_result" : aggregation_result
+        "aggregation_result": aggregation_result,
+        "annotate_queryset": list(annotate_queryset),
     }
     return render(request, "home.html", context)
 
