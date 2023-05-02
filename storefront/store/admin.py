65da9c933a01a890de890b5977094b71d642efd0
Ariel Carvalho Nascimento <arielcarvnasc@gmail.com>
2023-05-02 20:53:07 -0300

    refactor: Storefront - Admin custom fields

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -2,8 +2,24 @@ from django.contrib import admin
 
 from store.models import Product, Customer, Promotion, Order, Address, Collection, OrderItem, Cart
 
-admin.site.register(Product)
-admin.site.register(Customer)
+admin.AdminSite.site_header = "Storefront Admin"
+admin.AdminSite.site_title = "Storefront Admin Portal"
+admin.AdminSite.index_title = "Storefront Administration"
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "unit_price"]
+    list_editable = ["unit_price"]
+    list_per_page = 100
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ["membership"]
+
+
 admin.site.register(Promotion)
 admin.site.register(Order)
 admin.site.register(Address)
