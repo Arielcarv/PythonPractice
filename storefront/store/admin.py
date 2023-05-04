e9755bd97aa3d05c4e5a6ce4083030a676c9c118
Ariel Carvalho Nascimento <arielcarvnasc@gmail.com>
2023-05-04 18:31:41 -0300

    refactor: Storefront - Admin Adding computed columns.

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -21,9 +21,17 @@ class OrderItemAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["title", "unit_price"]
+    list_display = ["title", "unit_price", "inventory_status"]
     list_editable = ["unit_price"]
     list_per_page = 100
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product):
+        if product.inventory > 10:
+            return "OK"
+        if 0 <= product.inventory <= 10:
+            return "Low"
+        return "Out"
 
 
 admin.site.register(Address)
