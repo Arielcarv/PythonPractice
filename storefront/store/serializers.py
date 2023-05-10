bf740040aad61ac6f4197b7d74b00a0d0f2069a4
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-05-09 21:27:48 -0300

    feat: Storefront - RestAPI - Model serializers

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -3,32 +3,18 @@ from rest_framework import serializers
 from store.models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title"]
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=10, decimal_places=2, source="unit_price")
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "title", "unit_price", "price_with_taxes", "collection"]
+
     price_wit_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-
-    """ # PrimaryKeys
-    collection = serializers.PrimaryKeyRelatedField(
-        queryset=Collection.objets.all()
-    ) """
-
-    """ # String Relationship
-    collection = serializers.StringRelatedField() """
-
-    """ # Nested Objects Relationship
-    collection = CollectionSerializer() """
-
-    # Hyperlink Relationship
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(), view_name="collection_detail"
-    )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
