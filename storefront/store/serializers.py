fbca2fb4d3b6e8ca1a38c2846f325db7c5bf59ac
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-05-09 21:09:34 -0300

    feat: Storefront - RestAPI - Serializing Relationships

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -1,5 +1,11 @@
-from rest_framework import serializers
 from decimal import Decimal
+from rest_framework import serializers
+from store.models import Product, Collection
+
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 
 
 class ProductSerializer(serializers.Serializer):
@@ -7,6 +13,14 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=10, decimal_places=2, source="unit_price")
     price_wit_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+    # collection = serializers.PrimaryKeyRelatedField(
+    #     queryset=Collection.objets.all()
+    # )
+    # collection = serializers.StringRelatedField()
+    # collection = CollectionSerializer()
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Collection.objects.all(), view_name="collection_detail"
+    )
 
-    def calculate_tax(self, product):
+    def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
