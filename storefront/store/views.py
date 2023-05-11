b535692dc280a95e034ea7db407a547c471e81d9
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-05-10 23:39:49 -0300

    feat: Storefront - RestAPI - Deleting Objects

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -18,16 +18,25 @@ def product_list(request):
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     if request.method == "GET":
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    serializer = ProductSerializer(product, data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data)
+
+    if request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    if request.method == "DELETE":
+        if product.orderitem_set.count() > 0:
+            return Response({"error": "Product cannot be deleted because it's associated with an order item."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        serializer = ProductSerializer(product)
+        product.delete()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view()
