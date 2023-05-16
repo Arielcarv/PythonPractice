e248ae290b0cfbb1b875eaf14b63a4c1f1508856
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-05-16 20:34:51 -0300

    feat: Storefront - RestAPI - Nested Routers

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from store.views import ProductViewSet, CollectionViewSet
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+from store.views import CollectionViewSet, ProductViewSet, ReviewViewSet
 
 router = DefaultRouter()
 router.register("products", ProductViewSet, basename="products")
 router.register("collections", CollectionViewSet, basename="collections")
 
+products_router = NestedDefaultRouter(router, r"products", lookup="product")
+products_router.register(r"reviews", ReviewViewSet, basename="reviews")
+
 urlpatterns = [
-    path("", include(router.urls)),
+    path(r"", include(router.urls)),
+    path(r"", include(products_router.urls)),
 ]
