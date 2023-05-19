3c09aec2576416c4ed66f8176ad3229b08661462
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-05-18 22:06:56 -0300

    feat: Storefront - RestAPI - Sorting

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from store.models import Collection, OrderItem, Product, Review
@@ -11,9 +11,10 @@ from store.filters import ProductFilter
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
     search_fields = ["title", "description"]
+    ordering_fields = ["unit_price", "last_update"]
 
     def get_serializer_class(self):
         return ProductSerializer
