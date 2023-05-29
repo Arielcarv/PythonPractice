c2d5439e28778805a2d19221be84f2ba05561d01
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-05-29 20:32:30 -0300

    feat: Storefront - Implement django-environ & add remote database

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -10,18 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
+
 from datetime import timedelta
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+# environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+environ.Env.read_env(Path.joinpath(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^0qb33h#3a1(a54s!_l9v$a6!&5d+t17&#j)++)#h9=sx(!#@o"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -95,10 +102,10 @@ INTERNAL_IPS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "storefront",
-        "USER": "ariel",
-        "PASSWORD": "123456",
-        "HOST": "127.0.0.1",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST"),
         "PORT": "3306",
     }
 }
