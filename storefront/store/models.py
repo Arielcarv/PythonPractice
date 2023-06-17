a7252a0c87f534ce1088ff7ddef6e5fc30a4a85b
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-06-17 14:36:26 -0300

    feat: Storefront - RestAPI - Part III - Validating Uploaded Files

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
+from store.validators import validate_file_size
 from uuid import uuid4
 
 
@@ -55,7 +56,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to="store/images")
+    image = models.ImageField(upload_to="store/images", validators=[validate_file_size])
 
 
 class Customer(models.Model):
