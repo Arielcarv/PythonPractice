51306fe0586d1aaa15147fdd88e6101d3208a336
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-06-29 20:44:42 -0300

    feat: Storefront - RestAPI - Part III - Authenticating the User

--- storefront/store/tests/test_collections.py
+++ storefront/store/tests/test_collections.py
@@ -1,8 +1,11 @@
+import pytest
 from rest_framework.test import APIClient
 from rest_framework import status
 
 
+# @pytest.mark.djangodb
 class TestCreateCollection:
+    @pytest.mark.skip
     def test_if_user_is_annonymous_returns_401(self):
         # AAA (Arrange Act, Assert)
         # Arrange
@@ -13,3 +16,10 @@ class TestCreateCollection:
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post("/store/collections/", {"title": "a"})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
