023f15f30e3f0f58129c1a50cd1a06b3a0e0b0ab
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-07-05 21:44:18 -0300

    feat: Storefront - RestAPI - Part III - Running a Test Script

--- storefront/locustfiles/browse_products.py
+++ storefront/locustfiles/browse_products.py
@@ -3,20 +3,27 @@ from random import randint
 
 
 class SiteUser(HttpUser):
+    """
+    To run the test on a terminal: 'locust -f locustfiles/browse_products.py'
+    """
+
     wait_time = between(1, 5)
 
     @task(2)
     def view_products(self):
+        print("View Products")
         collection_id = randint(2, 6)
         self.client.get(f"/store/products/?collection_id={collection_id}", name="/store/products")
 
     @task(4)
     def view_product(self):
+        print("View Products Details")
         product_id = randint(1, 1000)
-        self.client.get(f"/storee/products/{product_id}", name="/store/products/:id")
+        self.client.get(f"/store/products/{product_id}", name="/store/products/:id")
 
     @task(1)
     def add_to_cart(self):
+        print("Add to Cart")
         product_id = randint(1, 10)
         self.client.post(
             f"/store/carts/{self.cart_id}/items/",
