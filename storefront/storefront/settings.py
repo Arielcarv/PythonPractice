e0150789d074a8b31e5c749d089799197634f6c3
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-07-28 19:16:36 -0300

    feat: Storefront - RestAPI - Part III - Add Procfile

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "*.a.run.app"]
 
 
 # Application definition
