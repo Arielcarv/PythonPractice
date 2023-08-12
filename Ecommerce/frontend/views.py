d80c7d57a0968276c419ffe4bdada5ce811cb20a
Ariel Carvalho <arielcarvnasc@gmail.com>
2023-08-12 14:03:17 -0300

    feat: Ecommerce - add and organize templates.

--- Ecommerce/frontend/views.py
+++ Ecommerce/frontend/views.py
@@ -4,7 +4,5 @@ from .models import Item
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    context = {
-        "items": Item.objects.all()
-    }
-    return render(request, "home.html", context=context)
+    context = {"items": Item.objects.all()}
+    return render(request, "frontend/home.html", context=context)
